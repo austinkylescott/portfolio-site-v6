88ff85e0734189b5cd5a7d31305277025641f310
austinkylescott <46319394+austinkylescott@users.noreply.github.com>
2025-10-23 12:59:03 -0400

    Reworked bits on homepage, updated readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+# Hey, I'm Austin Scott
+
+I built this portfolio on Nuxt using Nuxt UI. After being mired in React _(more specifically, the proprietary mess of Arc Fusion & Pagebuilder)_, Vue is quickly becoming a favorite framework of mine, mainly because of how easy it is to reason about after time away.
+
+Learning Vue has been a nice reminder that programming can be fun again and that I can use whatever I want for my own projects 🤷🏾‍♂️
+
+Check out the site and see where I've put my own spin on things.
+
 # Nuxt Minimal Starter
 
 Look at the [Nuxt documentation](https://nuxt.com/docs/getting-started/introduction) to learn more.
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import type { ButtonProps, PageLogosItems } from "@nuxt/ui";
+import type { ButtonProps } from "@nuxt/ui";
 
 const links = ref<ButtonProps[]>([
   {
-    label: "Work Experience",
-    to: "/about",
-    icon: "i-lucide-square-play",
+    label: "LinkedIn",
+    to: "https://www.linkedin.com/in/austinkylescott",
+    target: "_blank",
+    leadingIcon: "streamline-logos:linkedin-logo-block",
   },
   {
     label: "See projects",
@@ -19,38 +20,38 @@ const links = ref<ButtonProps[]>([
 
 const items = [
   {
-    label: "HTML5",
-    name: "vscode-icons:file-type-html",
+    alt: "HTML5",
+    src: "vscode-icons:file-type-html",
   },
   {
-    label: "CSS3",
-    name: "vscode-icons:file-type-css2",
+    alt: "CSS3",
+    src: "vscode-icons:file-type-css2",
   },
   {
-    label: "JavaScript",
-    name: "vscode-icons:file-type-js-official",
+    alt: "JavaScript",
+    src: "vscode-icons:file-type-js-official",
   },
   {
-    label: "SCSS",
-    name: "vscode-icons:file-type-scss",
+    alt: "SCSS",
+    src: "vscode-icons:file-type-scss",
   },
   {
-    label: "React.js",
-    name: "vscode-icons:file-type-reactjs",
+    alt: "React.js",
+    src: "vscode-icons:file-type-reactjs",
   },
   {
-    label: "Next.js",
-    name: "vscode-icons:file-type-next",
+    alt: "Next.js",
+    src: "vscode-icons:file-type-next",
   },
   {
-    label: "Vue.js",
-    name: "vscode-icons:file-type-vue",
+    alt: "Vue.js",
+    src: "vscode-icons:file-type-vue",
   },
   {
-    label: "Nuxt.js",
-    name: "vscode-icons:file-type-nuxt",
+    alt: "Nuxt.js",
+    src: "vscode-icons:file-type-nuxt",
   },
-] satisfies PageLogosItems;
+];
 </script>
 <template>
   <UPageHero
@@ -72,8 +73,8 @@ const items = [
 
     <UPageLogos :marquee="{ pauseOnHover: true }" :items="items">
       <template v-for="item in items" :key="item.label">
-        <UTooltip :text="item.label" :delay-duration="300">
-          <UIcon :name="item.name" class="size-14 shrink-0" />
+        <UTooltip :text="item.alt" :delay-duration="300">
+          <UIcon :name="item.src" class="size-14 shrink-0" />
         </UTooltip>
       </template>
     </UPageLogos>
