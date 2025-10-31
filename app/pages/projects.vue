d3439c719de2f3fd10a000d468ff2d0f974cddee
austinkylescott <46319394+austinkylescott@users.noreply.github.com>
2025-10-31 19:40:38 -0400

    Client render alerts so that I can change color based on light/dark mode w/no hydration erros

--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -15,24 +15,29 @@ const featured = computed(() =>
     <SectionsHireMe />
     <UPageBody>
       <h2 class="text-2xl font-bold mb-4">Featured Projects</h2>
-      <UAlert
-        title="These projects are finished projects."
-        description="I consider these mostly complete. I'll still come back to them from time to time, but the site is production-ready in its current state."
-        icon="i-lucide-circle-check"
-        color="success"
-        variant="solid"
-      />
+      <ClientOnly>
+        <UAlert
+          title="These projects are finished projects."
+          description="I consider these mostly complete. I'll still come back to them from time to time, but the site is production-ready in its current state."
+          icon="i-lucide-circle-check"
+          color="success"
+          :variant="$colorMode.value === 'dark' ? 'subtle' : 'solid'"
+        />
+      </ClientOnly>
       <UPageGrid class="gap-6">
         <ProjectCard v-for="p in featured" :key="p.title" :project="p" />
       </UPageGrid>
       <h2 class="text-2xl font-bold mb-4">Work in Progress</h2>
-      <UAlert
-        title="These are works in progress."
-        description="I'm currently working on these projects. They are in various phases of iteration."
-        icon="i-lucide-triangle-alert"
-        color="warning"
-        variant="solid"
-      />
+      <ClientOnly>
+        <UAlert
+          title="These are works in progress."
+          description="I'm currently working on these projects. They are in various phases of iteration."
+          icon="i-lucide-triangle-alert"
+          color="warning"
+          :variant="$colorMode.value === 'dark' ? 'subtle' : 'solid'"
+          :class="$colorMode.value === 'light' ? 'text-default' : null"
+        />
+      </ClientOnly>
       <UPageGrid class="gap-6">
         <ProjectCard v-for="p in wip" :key="p.title" :project="p" />
       </UPageGrid>
